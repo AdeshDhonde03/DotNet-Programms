9fdc5877251bba7432cf6f34e6b42d7674f0f5cf
AdeshDhonde03 <adeshdhonde03@gmail.com>
2025-03-19 18:01:47 +0530

    Add another code
    Add cosdes

---
+++ C#/OutParameter/OutParameter/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number ;
+        int number2 = 15;
+        Console.WriteLine(Addition(out number, number2));
+        Console.WriteLine(number);
+        Console.ReadLine();
+    }
+    static int Addition(out int number,int number2)
+    {
+        number = 10;
+        return number + number2;
+    }
+}
